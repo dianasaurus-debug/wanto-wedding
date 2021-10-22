2a503032286bb257037e4bf93181b96daf06bae9
Diana Fitri <diana.fitri@uisi.ac.id>
2021-10-22 23:28:53 +0700

    Update relasi di model product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,4 +16,8 @@ class Product extends Model
     {
         return $this->hasMany(MediaProduct::class, 'product_id', 'id');
     }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by', 'id');
+    }
 }
